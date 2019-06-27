21d4eaf92b1690f7a7f3a4623caa947e2404bc59
anewbegin95 <alex.newbegin95@gmail.com>
2019-06-27 07:04:00 -0400

    further work creating a dictionary with all ticker close data

--- build/001_hist_data_to_csv.py
+++ build/001_hist_data_to_csv.py
@@ -1,19 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Monday Jan 21 16:14:04 2019
+# Created on Wed Apr  3 09:01:42 2019
+# @author: anewbegin
+# Step 1: Import modules and get CWD & predecesors
 
-@author: anewbegin
-
-
-Notes:
-There is no easy way to do RelPath in Python!
-I wonder if there's a way to automate this to have it look at the # o/ hops
-btwn the target and source paths and have 
-"""
-"""
-Step 1: Import modules and get CWD & predecesors
-"""
-import os, sys
+import os
+import sys
 import pandas as pd
 import requests
 import io
@@ -23,15 +14,15 @@ parentDir = os.path.dirname(fileDir)
 gparentDir = os.path.dirname(parentDir)
 
 print(fileDir)
-#print(parentDir)
-#print(gparentDir)
+print(parentDir)
+print(gparentDir)
 
-#%% Step 2: Read in ticker config file - list of tickers
+# %% Step 2: Read in ticker config file - list of tickers
 filepath = r'data/001-vanguard_etf_list/cln/etf_list.csv'
 filename = os.path.join(fileDir, filepath)
 
 colnames = ['ticker', 'name', 'asset_class', 'subclass']
-data = pd.read_csv(filename, names=colnames, encoding = 'ISO-8859-1')
+data = pd.read_csv(filename, names=colnames, encoding='ISO-8859-1')
 
 tickers = data.ticker.tolist()
 del tickers[0]
@@ -41,49 +32,38 @@ print(filename)
 print(data)
 print(tickers)
 
-#%% Step 3: Build master Adj. Close DataFrame
+# %% Step 3: Build ticker dictionary
 dt1 = "20110131"
 dt2 = "20190531"
+ticker_dict = {"Date": pd.date_range(start=dt1, end=dt2, freq='M')[::-1],
+               "A": tickers
+               }
+adjCloseDict = dict.fromkeys(tickers)
+# print(ticker_dict)
+print(adjCloseDict)
 
-colnames = ['Date'] + tickers
-masterAdjClose = pd.DataFrame([], columns=colnames)
+# %% Step 4: Download data and zip to dictionary
 
-masterAdjClose['Date'] = pd.date_range(start=dt1, end=dt2, freq='M')
-masterAdjClose = masterAdjClose.sort_values(by='Date', ascending=False)
-
-#print(masterAdjClose.head())
-
-
-#%% Step 4: Read Adj. Close data into DataFrame from STOOQ.com url
 for ticker in tickers[0:10]:
-    url = 'https://stooq.com/q/d/l/?s=%s.us&d1=%s&d2=%s&i=m&o=1100000' % (ticker, dt1, dt2)
-#    filename = r'C:\Users\anewbegin\Documents\Personal\personalfinance\data\002-etf_hist_data\cln\%s.csv' % ticker.lower()
-    
+    url = r'https://stooq.com/q/d/l/?s=%s.us&d1=%s&d2=%s&i=m&o=1100000' \
+           % (ticker, dt1, dt2)
+
     urlReq = requests.get(url).content
     urlData = pd.read_csv(io.StringIO(urlReq.decode('utf-8')))
-#    print(urlData.head())
-    urlData = urlData.sort_values(by='Date', ascending=False)
 
-    for name in colnames:
-        if name == ticker:
-            masterAdjClose[name] = urlData['Close']
-#            masterAdjClose.name = masterAdjClose.name.astype(float)
-print(masterAdjClose.head())
+    urlCloseList = urlData['Close'].tolist()
 
-#%% Step 5: Print master Adj. Close DataFrame into .csv
-filepath = r'data\002-etf_hist_data\cln\masterAdjClose.csv'
-filename = os.path.join(gparentDir, filepath)
-masterAdjClose.to_csv(filename,index=False)
+#    print(urlCloseList[0:5])
 
-#%%
-#filepath = r'data\002-etf_hist_data\cln'
-#filename = os.path.join(gparentDir, filepath)
-#for the_file in os.listdir(filename):
-#    file_path = os.path.join(filename, the_file)
-#    try:
-#        if os.path.isfile(file_path):
-#            os.unlink(file_path)
-#        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
-#    except Exception as e:
-#        print(e)
+    for key in adjCloseDict.keys():
+        if ticker != key:
+            next
+        else:
+            adjCloseDict[ticker] = urlCloseList
+# print(adjCloseDict)
+# %% Clean / process data
+for data in my_empty_dict:
+    clean_the_data()
 
+# %% Combine
+master_data = combine_individual_data()
--- wp/wp006-url_to_dict/wp006-url_to_dict.py
+++ wp/wp006-url_to_dict/wp006-url_to_dict.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  3 09:01:42 2019
+# Created on Wed Apr  3 09:01:42 2019
+# @author: anewbegin
+# Step 1: Import modules and get CWD & predecesors
 
-@author: anewbegin
-"""
-"""
-Step 1: Import modules and get CWD & predecesors
-"""
-import os, sys
+import os
+import sys
 import pandas as pd
 import requests
 import io
@@ -20,12 +17,12 @@ print(fileDir)
 print(parentDir)
 print(gparentDir)
 
-#%% Step 2: Read in ticker config file - list of tickers
+# %% Step 2: Read in ticker config file - list of tickers
 filepath = r'data/001-vanguard_etf_list/cln/etf_list.csv'
 filename = os.path.join(fileDir, filepath)
 
 colnames = ['ticker', 'name', 'asset_class', 'subclass']
-data = pd.read_csv(filename, names=colnames, encoding = 'ISO-8859-1')
+data = pd.read_csv(filename, names=colnames, encoding='ISO-8859-1')
 
 tickers = data.ticker.tolist()
 del tickers[0]
@@ -39,24 +36,31 @@ print(tickers)
 dt1 = "20110131"
 dt2 = "20190531"
 ticker_dict = {"Date": pd.date_range(start=dt1, end=dt2, freq='M')[::-1],
-           "A": tickers
-           }
+               "A": tickers
+               }
 adjCloseDict = dict.fromkeys(tickers)
-#print(ticker_dict)
+# print(ticker_dict)
 print(adjCloseDict)
 
-# %% Download data
+# %% Step 4: Download data and zip to dictionary
 
-for ticker in tickers:
-    url = 'https://stooq.com/q/d/l/?s=%s.us&d1=%s&d2=%s&i=m&o=1100000' % (ticker, dt1, dt2)
+for ticker in tickers[0:10]:
+    url = r'https://stooq.com/q/d/l/?s=%s.us&d1=%s&d2=%s&i=m&o=1100000' \
+           % (ticker, dt1, dt2)
+
     urlReq = requests.get(url).content
     urlData = pd.read_csv(io.StringIO(urlReq.decode('utf-8')))
 
-#    filename = create_filename()
-#    data.to_csv(filename)
-#    my_empty_dict[ticker] = data
-    
-    
+    urlCloseList = urlData['Close'].tolist()
+
+#    print(urlCloseList[0:5])
+
+    for key in adjCloseDict.keys():
+        if ticker != key:
+            next
+        else:
+            adjCloseDict[ticker] = urlCloseList
+# print(adjCloseDict)
 # %% Clean / process data
 for data in my_empty_dict:
     clean_the_data()
